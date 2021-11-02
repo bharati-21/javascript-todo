3c0fe44cd05f353ed3a971cea6f0dbba05ffbc9a
Bharati Subramanian <bharatisharada@gmail.com>
2021-11-02 21:41:34 +0530

    feat: theme toggler

---
+++ src/theme-toggle.js
@@ -0,0 +1,13 @@
+const themeToggleCheckbox = document.querySelector('#theme-toggle-checkbox');
+const body = document.querySelector('body');
+themeToggleCheckbox.addEventListener('click', toggleTheme);
+
+function toggleTheme() {
+    if(body.classList.contains('light-theme')) {
+        body.className = 'dark-theme';
+    }
+    else {
+        body.className = 'light-theme';
+    }
+    console.log(body);
+}
