16b864be6f3d8941caf6ee2898da296473d91827
Bharati Subramanian <bharatisharada@gmail.com>
2021-11-02 23:32:45 +0530

    feat: count active tasks

--- src/todo-list-header.js
+++ src/todo-list-header.js
@@ -9,3 +9,17 @@ const today = new Date();
 todoDate.innerText = `${days[today.getDay()]}, ${month[today.getMonth()]} ${today.getDate()}`;
 
 todoActive.innerText = "0 Tasks";
+
+function addNumActiveTasks() {
+    const activeTasks = calculateNumTasks();
+    todoActive.innerText = `${activeTasks} ${activeTasks === 1 ? 'Task' : 'Tasks'}`;
+}
+
+function calculateNumTasks() {
+    let activeTasks = 0;
+    const children = Array.from(todoList.children);
+    children.forEach(child => {
+        activeTasks += child.classList.contains('active') ? 1: 0;
+    })
+    return activeTasks;
+}
